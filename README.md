7cdcff4e93ad8369f40565b39dd97b483357caa7
Jason Casares <jasonc@csdrafting.com>
2016-02-02 14:39:19 -0800

    Add README.md and changes per grader to js/app.js

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+### How to Start Game
+1. Start a **Static Server** using Python's 'SimpleHTTPSever or open file index.html
+
+## How to Play
+1.  Use arrow keys 'up', 'down', 'left', and 'right' to control 
+    hero.
+2.	Control hero and reach the water
+3.  Avoid all enemy bugs
+4.  When hero collides with enemy bugs, game will restart
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function (x, y) {
     this.x = x;
     this.y = y;
     this.speed = Math.floor((Math.random() * 5) + 1);
@@ -14,36 +14,37 @@ var Enemy = function(x, y) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     this.x = this.x + 100 * dt * this.speed;
-
-    if (this.x > 800) {
+    // Limit enemy reaches on X Axis before it resets
+    var enemyLimit = 800;
+    if (this.x > enemyLimit) {
         this.reset();
     }
 
-Enemy.prototype.reset = function() {
-    this.x = -50;
-    var valueY = [60, 140, 225];
-    this.y = valueY[Math.floor(Math.random() * 3)];
-    this.speed = Math.floor((Math.random() * 5) + 1);
-};
+    Enemy.prototype.reset = function () {
+        this.x = -50;
+        var valueY = [60, 140, 225];
+        this.y = valueY[Math.floor(Math.random() * 3)];
+        this.speed = Math.floor((Math.random() * 5) + 1);
+    };
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-var Player  = function() {
+var Player = function () {
     this.x = 200;
     this.y = 400;
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function() {
+Player.prototype.update = function () {
     if (this.x < 0) {
         this.x = 0;
     } else if (this.x > 400) {
@@ -53,30 +54,30 @@ Player.prototype.update = function() {
     } else if (this.y < 0) {
         this.y = 0;
     } else if (this.y > 400) {
-        this.y = 400;    
+        this.y = 400;
     }
 
     this.checkCollision();
 };
 
-Player.prototype.render = function() {
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(key) {
+Player.prototype.handleInput = function (key) {
     switch (key) {
-        case 'left':
-            this.x = this.x - 100;
-            break;
-        case 'up':
-            this.y = this.y -85;
-            break;
-        case 'right':
-            this.x = this.x + 100;
-            break;
-        case 'down':
-            this.y = this.y +85;
-            break;
+    case 'left':
+        this.x = this.x - 100;
+        break;
+    case 'up':
+        this.y = this.y - 85;
+        break;
+    case 'right':
+        this.x = this.x + 100;
+        break;
+    case 'down':
+        this.y = this.y + 85;
+        break;
     }
 };
 
@@ -85,10 +86,10 @@ Player.prototype.checkCollision = function () {
         if (this.x < allEnemies[i].x + 50 &&
             this.x + 50 > allEnemies[i].x &&
             this.y < allEnemies[i].y + 50 &&
-            this.y +50 > allEnemies[i].y) {
-                this.x = 200;
-                this.y = 400;
-    }
+            this.y + 50 > allEnemies[i].y) {
+            this.x = 200;
+            this.y = 400;
+        }
 };
 
 // Now write your own player class
@@ -99,22 +100,25 @@ Player.prototype.checkCollision = function () {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-var valueY = [60, 140, 225];
+(function() {
+    var allEnemies = [];
+    var valueY = [60, 140, 225];
 
-for (var i = 0; i < 5; i++) {
-    var x = (Math.floor(Math.random() * -1000) + 1);
-    var y = valueY[Math.floor(Math.random() * 3)];
-    var enemy = new Enemy(x,y);
-    allEnemies.push(enemy);
-}
+    for (var i = 0; i < 5; i++) {
+        var x = (Math.floor(Math.random() * -1000) + 1);
+        var y = valueY[Math.floor(Math.random() * 3)];
+        var enemy = new Enemy(x, y);
+        allEnemies.push(enemy);
+    window['allEnemies'] = allEnemies;
+    }
+})();
 
 var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
